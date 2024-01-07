bd3b30644753a512f159b20a7723bedde81eccd2
alexisforestier <alforestier@gmail.com>
2024-01-07 18:19:15 +0100

    Now fit functions and cstes in a Physics module. Clearing plots is working.

---
+++ h5temperature.py
@@ -0,0 +1,243 @@
+import sys
+import numpy as np
+import pandas as pd
+import h5py
+import matplotlib.pyplot
+import matplotlib
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
+from PyQt5.QtWidgets import (QApplication, 
+                             QWidget,
+                             QLabel,
+                             QSpinBox,
+                             QCheckBox,
+#                            QDoubleSpinBox,
+                             QGroupBox,
+                             QPushButton,
+                             QListWidget,
+                             QFormLayout,
+                             QVBoxLayout,
+                             QHBoxLayout,
+                             QFileDialog,
+                             QMessageBox)
+
+from h5temperature_Physics import planck, wien, temp2color
+
+class PlotsCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None):
+
+        self.fig, self.axes = matplotlib.pyplot.subplots(2, 2, 
+                            constrained_layout=True)
+
+        # Planck
+        self.axes[0,0].set_xlabel('wavelength (nm)')
+        self.axes[0,0].set_ylabel('intensity (arb. unit)')
+        self.ax_planck_res = self.axes[0,0].twinx()
+        self.ax_planck_res.set_ylabel('Planck fit residuals')
+
+        # Wien
+        self.axes[1,0].set_xlabel('1/wavelength (1/nm)')
+        self.axes[1,0].set_ylabel('Wien')
+        self.ax_wien_res = self.axes[1,0].twinx()
+        self.ax_wien_res.set_ylabel('Wien fit residuals')
+
+        # Two color
+        self.axes[0,1].set_xlabel('wavelength (nm)')
+        self.axes[0,1].set_ylabel('two-color temperature (K)')
+
+        # Two color Histogram
+        self.axes[1,1].set_xlabel('two-color temperature (K)')
+        self.axes[1,1].set_ylabel('frequency')
+
+        super(PlotsCanvas, self).__init__(self.fig)
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.resize(1500,900)
+        
+        # data stored in self
+        self.filepath = str()
+        self.data = dict()
+
+        # LAYOUTS
+
+        # left layout   
+        load_button = QPushButton('Load h5')
+        reload_button = QPushButton('Reload')
+        clear_button = QPushButton('Clear')
+
+        topleftbuttonslayout = QHBoxLayout()
+        topleftbuttonslayout.addWidget(load_button)
+        topleftbuttonslayout.addWidget(reload_button)
+
+        currentfile_layout = QHBoxLayout()
+        currentfile_label = QLabel('Current file:')
+        self.currentfilename_label = QLabel('')
+
+        currentfile_layout.addWidget(currentfile_label)
+        currentfile_layout.addWidget(self.currentfilename_label)
+
+        self.dataset_list = QListWidget()
+        self.dataset_list.setSelectionMode(1) # single selection
+
+        leftlayout = QVBoxLayout()
+        leftlayout.addLayout(topleftbuttonslayout)
+        leftlayout.addLayout(currentfile_layout)
+        leftlayout.addWidget(self.dataset_list)
+        leftlayout.addWidget(clear_button)
+
+        left_groupbox = QGroupBox('Data')
+        left_groupbox.setLayout(leftlayout)
+
+        left_groupbox.setMinimumWidth(230)
+
+        # right layout
+        lowerbound_spinbox = QSpinBox()
+        upperbound_spinbox = QSpinBox()
+        delta_spinbox = QSpinBox()
+
+        lowerbound_spinbox.setMinimum(400)
+        upperbound_spinbox.setMinimum(400)
+        delta_spinbox.setMinimum(1)
+        lowerbound_spinbox.setMaximum(1200)
+        upperbound_spinbox.setMaximum(1200)
+        delta_spinbox.setMaximum(1000)
+        # default values:
+        lowerbound_spinbox.setValue(400)
+        upperbound_spinbox.setValue(800)
+        delta_spinbox.setValue(100)
+
+        bg_checkbox = QCheckBox('Fit Planck background')
+
+        choosedelta_button = QPushButton('Choose delta')
+        fit_button = QPushButton('Fit')
+        results_button = QPushButton('Results')
+
+        fitparam_form = QFormLayout()
+        fitparam_form.addRow('Lower limit (nm):', lowerbound_spinbox)
+        fitparam_form.addRow('Upper limit (nm):', upperbound_spinbox)
+        fitparam_form.addRow('2-color delta (px):', delta_spinbox)
+        
+        fit_layout = QVBoxLayout()
+        fit_layout.addLayout(fitparam_form)
+        fit_layout.addWidget(choosedelta_button)
+        fit_layout.addWidget(bg_checkbox)
+        fit_layout.addWidget(fit_button)
+        fit_layout.addWidget(results_button)
+        fit_layout.addStretch()
+
+        right_groupbox = QGroupBox('Fit parameters')
+        right_groupbox.setLayout(fit_layout)
+        right_groupbox.setMinimumWidth(200)
+
+
+        # center layout
+        center_groupbox = QGroupBox()
+        center_groupbox.setStyleSheet('QGroupBox  {border: 2px solid gray;\
+                                                background-color: white;}')
+        plot_layout = QVBoxLayout()
+
+        # set empty plots
+        self.canvas = PlotsCanvas(self)
+        self.toolbar = NavigationToolbar2QT(self.canvas, self)
+        self.toolbar.setStyleSheet("font-size: 16px;")
+        plot_layout.addWidget(self.toolbar)
+        plot_layout.addWidget(self.canvas)
+
+        center_groupbox.setLayout(plot_layout)
+
+        layout = QHBoxLayout()
+        layout.addWidget(left_groupbox, stretch=2)
+        layout.addStretch()
+        layout.addWidget(center_groupbox, stretch=10)
+        layout.addStretch()
+        layout.addWidget(right_groupbox, stretch=1)
+        
+        self.setLayout(layout)
+
+
+        # CONNECTS
+
+        load_button.clicked.connect(self.load_h5file)
+        clear_button.clicked.connect(self.clear_alldata)
+
+        self.dataset_list.currentTextChanged.connect(self.update_plots)
+
+
+    def load_h5file(self):
+        options = QFileDialog.Options()
+        # ! use Native dialog or qt dialog... 
+        # ! Must be checked on different platforms
+    #   options |= QFileDialog.DontUseNativeDialog
+        self.filepath, _ = QFileDialog.getOpenFileName(self,
+            "Load HDF5 file", "","HDF5 file (*.h5)", 
+            options=options)
+
+        if self.filepath != str():
+            # read h5 file and store in self.data: 
+            file = h5py.File(self.filepath, 'r')
+            for nam, dat in file.items():
+                # get temperature measurements only
+                if 'measurement/T_planck' in dat:
+                    self.data[nam] = dat['measurement']
+
+            if len(self.data) == 0:
+                QMessageBox.about(self, 
+                    "Message", "No temperature data available in HDF5 file.")
+                self.filepath = str()
+            else:
+                self.currentfilename_label.setText(
+                                        self.filepath.split('/')[-1])
+                self.dataset_list.addItems(self.data.keys())
+
+
+    def clear_alldata(self):
+        if self.filepath != str():
+            self.filepath = str()
+            self.currentfilename_label.setText('')
+            self.data = dict()
+            self.dataset_list.clear()
+
+
+    def update_plots(self, nam):
+        if nam:
+
+            self.canvas.axes[0,0].collections.clear()
+            self.canvas.axes[1,0].collections.clear()
+            self.canvas.axes[0,1].collections.clear()
+            _ = [b.remove() for b in self.canvas.axes[1,1].containers]
+
+            x = np.array( self.data[nam]['spectrum_lambdas'] )
+            y_planck = np.array( self.data[nam]['planck_data'] )
+            y_wien = wien(y_planck, x)
+            y_2c = temp2color(x, y_wien, 100)
+    
+            self.canvas.axes[0,0].scatter(x, 
+                                          y_planck, 
+                                          c='gray', 
+                                          s=10, 
+                                          label='Planck data')
+            self.canvas.axes[1,0].scatter(1/x, 
+                                          y_wien, 
+                                          c='gray', 
+                                          s=10, 
+                                          label='Wien data')
+            self.canvas.axes[0,1].scatter(x[:-100], 
+                                          y_2c, 
+                                          c='gray', 
+                                          s=10, 
+                                          label='Two-color data')
+    
+            self.canvas.axes[1,1].hist(y_2c, 
+                                       bins = int(len(y_2c)/10),
+                                       alpha=0.3, 
+                                       label='two-color histogram')
+            self.canvas.draw()
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+app.exec()
---
+++ h5temperature_Physics.py
@@ -0,0 +1,186 @@
+import numpy as np
+
+h = 6.62607015e-34 # Planck's constant in J/s
+c = 299792458 # Speed of light in m/s
+k = 1.380649 * 1e-23 # Boltzmann constant in J/K
+
+# all function takes lamb in nm
+def planck(lamb, eps, temp, b):
+    lamb = lamb * 1e-9 # now in meters
+    f = ( eps * ( 2*np.pi*h*c**2 / (lamb**5) ) * 
+         1 / ( np.exp(h * c / (lamb * k * temp)) - 1 ) + b )
+    return f
+
+def wien(lamb, I):
+    lamb = lamb * 1e-9 # now in meters
+    f = (k / (h*c)) * np.log(2 * np.pi * h * c**2 / (I * lamb**5) )
+    return f 
+
+def temp2color(lamb, wien, deltapx):    
+    n = np.array( [ 1/lamb[k] - 1/lamb[k + deltapx]
+                    for k in range(len(lamb)-deltapx)] )
+    d = np.array( [wien[k] - wien[k + deltapx]
+                    for k in range(len(lamb)-deltapx)] )
+    return(1e9 * n/d)
+
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import h5py
+    from scipy.optimize import curve_fit
+    ############################################################################
+    # Inputs:
+    
+    # full path to h5 file:
+    file = h5py.File('/home/alex/mnt/Data1/ESRF/hc5078_10_13-02-2023-CDMX18/CDMX18/hc5078_CDMX18.h5', 'r')
+    
+    # scan to analyze:
+    scan = 'CDMX18_mesh01_18.1'
+    
+    # Planck fit interval in nm:
+    fit_interval = np.array([540, 920]) 
+    
+    # delta in px for two color temperature:
+    deltapx = 80
+    
+    
+    ############################################################################
+    
+    # read data in h5 file
+    x = np.array( file[scan]['measurement']['spectrum_lambdas'] )
+    y_planck = np.array( file[scan]['measurement']['planck_data'] )
+    y_planck_fit = np.array( file[scan]['measurement']['planck_fit'] )
+    
+    
+    in_interval = np.array( x > fit_interval[0] ) & np.array( x < fit_interval[1] ) 
+    x_tofit = x[in_interval]
+    y_planck_tofit = y_planck[in_interval]
+    
+    fig, axs = plt.subplots(2,2, figsize = (12, 9))
+    fig.suptitle(scan)
+    
+    
+    axs[0,0].set_xlim(fit_interval + np.array([-20, 20]))
+    axs[0,0].set_ylim([y_planck_tofit.min() - 0.3*y_planck_tofit.ptp(), 
+                     y_planck_tofit.max() + 0.3*y_planck_tofit.ptp()] )
+    
+    axs[0,0].scatter(x, y_planck, c='k', s=10, alpha=0.2, label='Planck data')
+    
+    axs[0,0].plot(x, y_planck_fit, 'r--', linewidth=.8, label='old fit')
+    
+    par, cov = curve_fit(planck, 
+                         x_tofit, 
+                         y_planck_tofit, 
+                         p0     = (    1e-6,   2000,        0),
+                         bounds =(( -np.inf,      0,        0),
+                                 (  +np.inf,    1e4,  +np.inf)),
+                         method = 'trf')
+    
+    axs[0,0].plot(x_tofit, planck(x_tofit, *par) , c='limegreen', linewidth=2, label='new fit')
+    
+    ax_planck_res = axs[0,0].twinx()
+    ax_planck_res.set_ylabel('Planck fit residuals')
+    axs[0,0].set_zorder(1)
+    axs[0,0].patch.set_visible(False)
+    
+    ax_planck_res.scatter(x_tofit, y_planck_tofit-planck(x_tofit, *par), 
+        c='lightgray', marker='v', s=5, label='residuals')
+    
+    
+    axs[0,0].set_xlabel('wavelength (nm)')
+    axs[0,0].set_ylabel('intensity')
+    
+    axs[0,0].text(0.4, 0.8, 'T$_\\mathrm{planck}$= ' + str( round(par[1]) ) + ' K', size=15, color='g',  transform=axs[0,0].transAxes)
+    
+    
+    
+    print('**********************************')
+    
+    print('Planck fit:')
+    
+    print('epsilon    = ', par[0], '  ')
+    print('T planck   = ', par[1], ' K')
+    print('background = ', par[2], '  ')
+    print('**********************************\n')
+    
+    
+    # Wien fit
+    
+    axs[1,0].set_xlabel('1/wavelength (1/nm)')
+    axs[1,0].set_ylabel('Wien')
+    
+    x_wien = 1/x_tofit
+    y_wien = wien(y_planck_tofit, x_tofit)
+    
+    axs[1,0].scatter(x_wien, y_wien, c='k', s=10, alpha=0.2, label='Wien data')
+    
+    a,b = np.polyfit(x_wien, y_wien, 1)
+    
+    axs[1,0].plot( x_wien, a * x_wien + b, c='limegreen', linewidth=2, label='fit')
+    axs[1,0]
+    
+    ax_wien_res = axs[1,0].twinx()
+    ax_wien_res.set_ylabel('Wien fit residuals')
+    axs[1,0].set_zorder(1)
+    axs[1,0].patch.set_visible(False)
+    
+    ax_wien_res.scatter( x_wien, y_wien - ( a * x_wien + b ), 
+        c='lightgray', marker='v', s=5, label='residuals')
+    
+    axs[1,0].text(0.3, 0.8, 'T$_\\mathrm{wien}$= ' + str( round(1e9 * 1/a) ) + ' K', size=15, color='g',  transform=axs[1,0].transAxes)
+    
+    
+    
+    # sliding two color
+    
+    temp2c = temp2color(x_tofit, y_wien, deltapx)
+    
+    axs[0,1].scatter(x_tofit[:-deltapx],  temp2c, c='k', s=5, alpha=0.2, label='two-color temperature')
+    
+    axs[0,1].set_ylim([temp2c.mean()- 8*temp2c.std(), temp2c.mean()+ 8*temp2c.std()])
+    
+    ax2_2c = axs[0,1].twiny()
+    ax2_2c.hist(temp2c, bins=int(len(temp2c)/8), orientation='horizontal', color='red', alpha=0.3, label='two-color histogram')
+    ax2_2c.set_xlim(np.array(ax2_2c.get_xlim()) *3)
+    ax2_2c.set_xlabel('frequency')
+    
+    axs[0,1].axhline(y = temp2c.mean(), color='limegreen', linewidth=2, zorder=-1)
+    axs[0,1].text(0.3, 0.8, 'T$_\\mathrm{two-color}$ = ' + str( round(temp2c.mean()) ) + ' K', size=15, color='g',  transform=axs[0,1].transAxes)
+    axs[0,1].text(0.3, 0.7, 'std dev. = ' + str( round(temp2c.std()) ) + ' K', size=15, color='g',  transform=axs[0,1].transAxes)
+    
+    axs[0,1].set_zorder(1)
+    axs[0,1].patch.set_visible(False)
+    
+    axs[0,1].set_xlabel('wavelength (nm)')
+    axs[0,1].set_ylabel('two-color temperature (K)')
+    
+    # two color T st deviation vs delta
+    deltas = np.array(list(range(300)))
+    stddevs = np.array( [ temp2color(x_tofit, y_wien, deltas[i]).std() for i in deltas ] )
+    
+    axs[1,1].scatter(deltas, stddevs, c='k', s=20, alpha=0.2, label='T$_\\mathrm{two-color}$ std dev.')
+    axs[1,1].axvline(x=deltapx, color='limegreen', linewidth=3)
+    axs[1,1].set_ylim([0, np.nanmean(stddevs)])
+    
+    axs[1,1].text(deltapx+10, np.nanmean(stddevs)/2, 'current $\\delta$ = ' + str(deltapx) + ' px', size=15, color='g')
+    axs[1,1].set_xlabel('$\\delta$ (px)')
+    axs[1,1].set_ylabel('T$_\\mathrm{two-color}$ std dev. (K)')
+    
+    
+    axs[0,0].legend()
+    axs[1,0].legend()
+    axs[0,1].legend(loc = 'lower right')
+    axs[1,1].legend()
+    
+    ax_planck_res.legend(loc='upper right')
+    ax_wien_res.legend(loc='upper right')
+    ax2_2c.legend(loc='upper right')
+    
+    fig.tight_layout()
+    plt.show(block=True)
+    
+    fig.savefig('./temp_analysis_' + scan + '.png')
+    
+    file.close()
+    
