de8927a6c2efb7c5519e3482486da52fb245fbd3
alexisforestier <alforestier@gmail.com>
2025-04-05 23:41:21 +0200

    update create_executable_windows, icon works

--- create_executable_windows.bat
+++ create_executable_windows.bat
@@ -1,11 +1,13 @@
-python -m venv .venv 
+python -m venv .venv
 call .\.venv\Scripts\activate.bat
 python -m pip install -r requirements.txt
 python -m pip install nuitka
-python -m nuitka --standalone --windows-icon-from-ico=.\h5temperature\h5temp.ico --enable-plugin=pyqt5 run.py
+python -m nuitka --standalone --windows-icon-from-ico=.\h5temperature\resources\h5temp.ico --enable-plugin=pyqt5 run.py
+@echo off
+echo COPY RESOURCES
 set "source=.\h5temperature\resources"
-cd .\run.dist\
-mkdir h5temperature
-cd h5temperature
-xcopy "%source%" . /E /I /Y
+set "dest=.\run.dist\h5temperature\resources"
+echo source  %source%
+echo dest  %dest%
+xcopy "%source%" "%dest%" /E /I /Y
 PAUSE
