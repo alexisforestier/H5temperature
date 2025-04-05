c670db25e03000a2ed253579afae4fe5a987b4f2
alexisforestier <alforestier@gmail.com>
2025-04-05 21:15:38 +0200

    try on create_executable_windows.bat

--- create_executable_windows.bat
+++ create_executable_windows.bat
@@ -2,5 +2,10 @@ python -m venv .venv
 call .\.venv\Scripts\activate.bat
 python -m pip install -r requirements.txt
 python -m pip install nuitka
-python -m nuitka --standalone --enable-plugin=pyqt5 run.py
+python -m nuitka --standalone --windows-icon-from-ico=.\h5temperature\h5temp.ico --enable-plugin=pyqt5 run.py
+set "source=.\h5temperature\resources"
+cd .\run.dist\
+mkdir h5temperature
+cd h5temperature
+xcopy "%source%" . /E /I /Y
 PAUSE
